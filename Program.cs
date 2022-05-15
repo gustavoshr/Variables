a2765a8f14855a9a9cb299c710114ad5256260e9
gustavoshr <gustavoriquesb2002@gmail.com>
2022-05-15 19:02:02 -0300

    variables

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int numero = 11;
+           int numero2 = 21;
+           Console.WriteLine(numero + " - " + numero2);
+
+           int numeroAC = numero + 1;
+           int numeroDec = numero2 - 1;
+           Console.WriteLine(numeroAC + " - " + numeroDec);
+
+        }
+    }
+}
